e98a6236337ff89d581b6a8adffe7f86b4c8f341
ahsan <ahsanfarooq210@gmail.com>
2021-06-09 12:51:42 +0500

    validation xaxis yaxis

--- src/LL1Parser.java
+++ src/LL1Parser.java
@@ -43,31 +43,87 @@ public class LL1Parser
         Grammer g25 = new Grammer("idset'", ", idset", (HashMap<String, String>) Map.of("idset", "idset"));
 
         table = new Grammer[][]{
-                /*          void    main    id  (     )   intlit  *    +     ;    cout  cin    int    <    >    ,      $*/
-                /*Cprogram*/{g1,   null, null,null, null, null, null, null, null, null, null, null, null, null, null, null},
-                /*AS*/      {null, null, g10, null, null, null, null, null, null, null, null, null, null, null, null, null},
-                /*E*/       {null, null, g11, g11,  null, g11,  null, null, null, null, null, null, null, null, null, null},
-                /*E'*/      {null, null, null,null, g19,  null, g18,  null, g19 , null, null, null, null, null, null, null},
-                /*F*/       {null, null, g14, g13 , null, g15 , null, null, null, null, null, null, null, null, null, null},
-                /*T*/       {null, null, g12, g12 , null, g12 , null, null, null, null, null, null, null, null, null, null},
-                /*T'*/      {null, null, null,null, g21 , null, g21 , g20 , g21 , null, null, null, null, null, null, null},
-                /*cinstmt*/ {null, null, null,null, null, null, null, null, null, null, g17 , null, null, null, null, null},
-                /*coutstmt*/{null, null, null,null, null, null, null, null, null, g16 , null, null, null, null, null, null},
-                /*decstmt*/ {null, null, null,null, null, null, null, null, null, null, null, g7  , null, null, null, null},
-                /*idset*/   {null, null, g9  ,null, null, null, null, null, null, null, null, null, null, null, null, null},
-                /*idset' */ {null, null, null,null, null, null, null, null, g24 , null, null, null, null, null, g25 , null},
-                /*stmt*/    {null, null, g4  ,null, null, null, null, null, null, g5  , g6 ,  g3  , null, null, null, null},
-                /*stmtset*/ {null, null, g2  ,null, null, null, null, null, null, g2  , g2 ,  g2  , null, null, null, null},
-                /*stmtwet'*/{null, null, g23 ,null, g22 , null, null, null, null, g23 , g23,  g23 , null, null, null, null},
-                /*type*/    {null, null, null,null, null, null, null, null, null, null, null, g8  , null, null, null, null}
+                /*          void    main    id  (     )   intlit  *    +     ;    cout  cin    int    <    >    ,      $      }       {   */
+                /*Cprogram*/{g1,   null, null,null, null, null, null, null, null, null, null, null, null, null, null, null,  null,  null},
+                /*AS*/      {null, null, g10, null, null, null, null, null, null, null, null, null, null, null, null, null,  null,  null},
+                /*E*/       {null, null, g11, g11,  null, g11,  null, null, null, null, null, null, null, null, null, null,  null,  null},
+                /*E'*/      {null, null, null,null, g19,  null, g18,  null, g19 , null, null, null, null, null, null, null,  null,  null},
+                /*F*/       {null, null, g14, g13 , null, g15 , null, null, null, null, null, null, null, null, null, null,  null,  null},
+                /*T*/       {null, null, g12, g12 , null, g12 , null, null, null, null, null, null, null, null, null, null,  null,  null},
+                /*T'*/      {null, null, null,null, g21 , null, g21 , g20 , g21 , null, null, null, null, null, null, null,  null,  null},
+                /*cinstmt*/ {null, null, null,null, null, null, null, null, null, null, g17 , null, null, null, null, null,  null,  null},
+                /*coutstmt*/{null, null, null,null, null, null, null, null, null, g16 , null, null, null, null, null, null,  null,  null},
+                /*decstmt*/ {null, null, null,null, null, null, null, null, null, null, null, g7  , null, null, null, null,  null,  null},
+                /*idset*/   {null, null, g9  ,null, null, null, null, null, null, null, null, null, null, null, null, null,  null,  null},
+                /*idset' */ {null, null, null,null, null, null, null, null, g24 , null, null, null, null, null, g25 , null,  null,  null},
+                /*stmt*/    {null, null, g4  ,null, null, null, null, null, null, g5  , g6 ,  g3  , null, null, null, null,  null,  null},
+                /*stmtset*/ {null, null, g2  ,null, null, null, null, null, null, g2  , g2 ,  g2  , null, null, null, null,  null,  null},
+                /*stmtset'*/{null, null, g23 ,null, null, null, null, null, null, g23 , g23,  g23 , null, null, null, null,  g22 ,  null},
+                /*type*/    {null, null, null,null, null, null, null, null, null, null, null, g8  , null, null, null, null,  null,  null}
 
         };
+    }
+    private int xAxis(String str)
+    {
+        return switch (str)
+                {
+                    case "void"->0;
+                    case "main"->1;
+                    case "id"->2;
+                    case "("->3;
+                    case ")"->4;
+                    case "intlit"->5;
+                    case "*"->6;
+                    case "+"->7;
+                    case ";"->8;
+                    case "cout"->9;
+                    case "cin"->10;
+                    case "int"->11;
+                    case "<"->12;
+                    case ">"->13;
+                    case ","->14;
+                    case "$"->15;
+                    case "}"->16;
+                    case "{"->17;
+                    default -> -1;
+                };
+    }
+    private int yAxis(String str)
+    {
+        return switch(str)
+                {
+                    case "cprogram"->0;
+                    case "AS"->1;
+                    case "E"->2;
+                    case "E'"->3;
+                    case "F"->4;
+                    case "T"->5;
+                    case "T'"->6;
+                    case "cinstmt"->7;
+                    case "coutstmt"->8;
+                    case "decstmt"->9;
+                    case "idset"->10;
+                    case "idset'"->11;
+                    case "stmt"->12;
+                    case "stmtset"->13;
+                    case "stmtset'"->14;
+                    case "type'"->15;
+                    default -> -1;
+
+                };
     }
 
     public void validate(ArrayList<String> list)
     {
         Stack <String> s=new Stack<>();
         s.push("cprogram");
+        boolean flag=false;
+        String inputPointer;
+        int pointerNumber=0;
+        while (true)
+        {
+
+        }
 
     }
 
